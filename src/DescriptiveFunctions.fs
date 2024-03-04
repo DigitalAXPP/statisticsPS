6614ae371cfd32474e42a389f9f5973ebf406dbf
Alexander Piepenhagen <a.piepenhagen@gmx.net>
2024-03-04 22:24:24 +0100

    New function 'getRelativeModeFromTable'.

--- src/DescriptiveFunctions.fs
+++ src/DescriptiveFunctions.fs
@@ -1,7 +1,7 @@
 ﻿//namespace statisticsPS
 module DescriptiveFunctions
     open FSharp.Stats
-
+    
 //module Say =
     let getDistributions =
         Distributions.Continuous.Normal.Init 25. 0.1
@@ -11,3 +11,15 @@ module DescriptiveFunctions
         table
         |> List.countBy id
         |> List.sortByDescending (fun (id, count) -> count)
+
+    let getRelativeModeFromTable table =
+        let length = table |> List.length
+        let (floatLength : float) = length
+        table 
+        |> List.groupBy id
+        |> List.map (fun (key, item) -> (key, System.Math.Round(((float)item.Length/floatLength)*100.00)))
+        |> List.sortByDescending (fun (_, item) -> item)
+        
+        
+        //|> List.length
+        //floatLength
--- statisticsPS.Tests/DescriptiveFunctions_Tests.fs
+++ statisticsPS.Tests/DescriptiveFunctions_Tests.fs
@@ -10,13 +10,25 @@ open DescriptiveFunctions
 [<TestFixture>]
 type TestClass () =
     [<Test>]
-    member this.TestGetMode () =
+    member this.TestGetAbsoluteMode () =
         //-- Arrange
-        let table = [ "Mercedes"; "BMW"; "Ford"; "Mercedes"; "BMW" ; "BMW"; "Ford"; "Mercedes"; "Mercedes"]
+        let table = [ "Mercedes"; "BMW"; "Ford"; "Mercedes"; "BMW" ; "BMW"; "Ford"; "Mercedes"; "Mercedes"; "Mercedes"]
 
         //-- Act
         let actual = getModeFromTable table
 
         //-- Assert
-        let expected = [("Mercedes", 4); ("BMW", 3); ("Ford", 2)]
+        let expected = [("Mercedes", 5); ("BMW", 3); ("Ford", 2)]
+        Assert.AreEqual(actual, expected)
+
+    [<Test>]
+    member this.TestGetRelativeMode () =
+        //-- Arrange
+        let table = [ "Mercedes"; "Mercedes"; "BMW"; "Ford"; "BMW" ; "BMW"; "Ford"; "Mercedes"; "Mercedes"; "Mercedes"]
+
+        //-- Act
+        let actual = getRelativeModeFromTable table
+
+        //-- Assert
+        let expected = [("Mercedes", 50.0); ("BMW", 30.0); ("Ford", 20.0)]
         Assert.AreEqual(actual, expected)
